e66920ebd1e042c6a26800267efe14ea47e49cac
jarethholt <jareth.holt@gmail.com>
2024-07-11 21:54:06 +0200

    Add the primary Blog class

---
+++ MyBlogger/Models/Blog.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlogger.Models;
+
+public class Blog
+{
+    public Guid BlogId { get; set; }
+    public required string UserId { get; set; }
+    public DateTime PostedAt { get; set; }
+    public string Header { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public string? ImageFileName { get; set; } = null;
+}
